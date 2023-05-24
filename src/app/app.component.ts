eb19f1ed64e98fdd54f0b1808015b3e11aa0e295
gaston becerra <gaston.becerra@gmail.com>
2023-05-24 01:48:32 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'evoc-json';
+  surveyName = 'x';
+  surveyIntroduction = 'y'
+  inputs = 0;
+  inputsArray = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('assets/config.json').subscribe(
+        (data: any) => {
+          this.surveyName = data.surveyInfo.surveyName;
+          this.surveyIntroduction = data.surveyInfo.surveyIntroduction;
+          this.inputs = data.evocations.inputs;
+          this.inputsArray = Array(this.inputs).fill(0).map((x,i)=>i);
+        },
+        (error: any) => {
+          console.error('Error loading configuration file');
+        }      
+    );
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatStepperModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
